03c1ce43e9a65a639783b27309c40d821b9cfff6
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2025-09-03 17:38:27 +0300

    Reorganisation on files

--- codes/main_menu.gd
+++ codes/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_button_down() -> void:
-	get_tree().change_scene_to_file("res://survivors_game.tscn")
+	get_tree().change_scene_to_file("res://scenes/survivors_game.tscn")
 
 
 func _on_exit_button_button_down() -> void:
